fd930e01fa8bbe0f2ba4d86f23d00d30f54cf3ee
suzukiryuuhei <kd1340410@st.kobedenshi.ac.jp>
2024-04-23 14:24:48 +0900

    error

---
+++ public/error.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>エラーPHP</title>
+</head>
+
+<body>
+    <?php
+    echo <p>Hello,World</p>;
+    ?>
+    <p>0J0X0XX神戸電子</p>
+</body>
+
+</html>
